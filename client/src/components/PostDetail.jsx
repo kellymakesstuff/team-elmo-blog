cfaba239edfcf7c3bd67c6e5d375fa94714f142e
Kelly Bee <kellybee@Kellys-iMac.home>
2020-06-16 16:40:47 -0400

    post detail page created, needs functionality

--- client/src/components/PostDetail.jsx
+++ client/src/components/PostDetail.jsx
@@ -4,11 +4,12 @@ import axios from 'axios'
 
 export default function PostDetail({ post }) {
   return (
-    <div className="postList">
-      <h1>Title: {post.title}</h1>
+    <div className="postDetail">
+      <h1>{post.title}</h1>
+      {/* @Felicia: I left the height and width here but you can change it to whatever you want so it's a main image like in a post  */}
+      <img src={post.img} height="200px" width="auto" />
       <h2>Author: {post.author}</h2>
       <p>{post.post}</p>
-      <img src={post.img} height="200px" width="auto" />
     </div>
   )
 }
