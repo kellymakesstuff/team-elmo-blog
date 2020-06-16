552af3fddb73b97a946faaf22d51aea035152377
Zachary Glassman <zach@Zacharys-MacBook-Air.local>
2020-06-16 16:40:34 -0400

    Search function

--- client/src/components/shared/Search.jsx
+++ client/src/components/shared/Search.jsx
@@ -1,31 +1,34 @@
-import React, { useEffect, useState} from 'react';
-import { Route, Switch, Link } from 'react-router-dom'
-import axios from 'axios'
-import PostList from "./src/components/PostList.jsx"
+import React, { useEffect, useState } from "react";
+import { Route, Switch, Link } from "react-router-dom";
+import axios from "axios";
+import PostList from "./src/components/PostList";
 
-export default function Search() {
-    return (
-        <div>
-            <input type="text" className="input" onChange={this.handleChange} placeholder="Search..." />
-        </div>
-    )
-}
-
-handleChange(e) {
-let PostList = [];
-let newList = [];
-if (e.target.value !== "") {
-PostList = this.props.items;
-
-newList = PostList.filter(item => {
-  const lc = item.toLowerCase();
-  const filter = e.target.value.toLowerCase();
-  return lc.includes(filter);
-});
-} else {
-newList = this.props.items;
-}
-this.setState({
-filtered: newList
-});
+export default function Search({ post, updatePost }) {
+  const handleChange = (e) => {
+    let PostList = [];
+    let newList = [];
+    if (e.target.value !== "") {
+      PostList = post.data;
+      newList = PostList.filter((item) => {
+        const lc = item.toLowerCase();
+        const filter = e.target.value.toLowerCase();
+        return lc.includes(filter);
+      });
+    } else {
+      newList = post.items;
+    }
+    useEffect(() => {
+      updatePost(newList);
+    }, []);
+  };
+  return (
+    <div>
+      <input
+        type="text"
+        className="input"
+        onChange={handleChange}
+        placeholder="Search..."
+      />
+    </div>
+  );
 }
