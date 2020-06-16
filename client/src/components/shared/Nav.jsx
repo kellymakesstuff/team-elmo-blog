ff43c3ab81bbdff53ec72a0e188d6f467b5cffd3
JoshR <joshuarizek@gmail.com>
2020-06-16 17:07:19 -0400

    working on addPost component

--- client/src/components/shared/Nav.jsx
+++ client/src/components/shared/Nav.jsx
@@ -3,6 +3,7 @@ import { Route, Switch, Link } from 'react-router-dom'
 import axios from 'axios'
 import {NavLink} from "react-router-dom"
 
+
 export default function Nav() {
     return (
         <nav>
--- client/src/services/posts.js
+++ client/src/services/posts.js
@@ -1,48 +1,48 @@
-import post from "./App"
-// import api from "./apiConfig"
+// import post from "./App"
+import api from "./apiSetup"
 
 
 export const getPosts = async () => {
   try {
-    const response = await post.get("/posts")
+    const response = await api.get("/posts")
     return response.data
   } catch (error) {
     throw error
   }
 }
 
-export const getPost = async() => {
-  try{
-    const response = await post.get(`/posts/${id}`)
-    return response.data
-  } catch (error) {
-    throw error
-  }
-}
+// export const getPost = async() => {
+//   try{
+//     const response = await post.get(`/posts/${id}`)
+//     return response.data
+//   } catch (error) {
+//     throw error
+//   }
+// }
 
-export const createPost = async posts => {
-  try {
-    const response = await post.post('/posts', posts)
-    return response.data
-  } catch (error) {
-    throw error
-  }
-}
+// export const createPost = async posts => {
+//   try {
+//     const response = await post.post('/posts', posts)
+//     return response.data
+//   } catch (error) {
+//     throw error
+//   }
+// }
 
-export const updatePost = async (id, posts) => {
-  try {
-    const response = await post.put(`/posts/${id}`, posts)
-    return response.data
-  } catch (error) {
-    throw error
-  }
-}
+// export const updatePost = async (id, posts) => {
+//   try {
+//     const response = await post.put(`/posts/${id}`, posts)
+//     return response.data
+//   } catch (error) {
+//     throw error
+//   }
+// }
 
-export const deletePost = async id => {
-  try {
-    const response = await post.delete(`/posts/${id}`)
-    return response.data
-  } catch (error) {
-    throw error
-  }
-}
+// export const deletePost = async id => {
+//   try {
+//     const response = await post.delete(`/posts/${id}`)
+//     return response.data
+//   } catch (error) {
+//     throw error
+//   }
+// }
