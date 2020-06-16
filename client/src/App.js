a407c27215a207e6b0ba806d5abb6cab75055c9a
bergkma <bergkma@gmail.com>
2020-06-16 12:12:42 -0400

    React dev (#7)
    * added API call and mapped Post component with data
    
    * api call running

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,35 @@ import { Route, Switch, Link } from 'react-router-dom'
 import axios from 'axios'
 import './App.css';
 
+//
 function App() {
+  const [post, updatePost] = useState([])
 
-  const [post, updatePost] = useState({
-    author: "Baby Yoda",
-    img: "https://miro.medium.com/max/1400/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg",
-    post: "I love cats.",
-    title: "Cats love me."
-  })
+  useEffect(() => {
+    apiCall()
+
+  }, [])
+
+  let apiCall = async () => {
+    let postData = await axios("https://blog-app-exercise-hw.herokuapp.com/api/posts")
+    console.log(postData.data)
+    updatePost(postData.data)
+  }
+
 
   return (
     <div className="app">
-      < Post post={post} />
+      {post.map(post => < Post post={post} />)}
+
     </div>
   );
 }
 
 export default App;
+
+// {
+//   author: "Baby Yoda",
+//   img: "https://miro.medium.com/max/1400/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg",
+//   post: "I love cats.",
+//   title: "Cats love me."
+// }
