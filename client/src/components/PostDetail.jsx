7e5d119ece7d0c6fb149f759dd0adef305042b90
zainsattar18 <zainsattar18@gmail.com>
2020-06-17 15:20:34 -0400

    post detail

--- client/src/components/PostDetail.jsx
+++ client/src/components/PostDetail.jsx
@@ -1,20 +1,31 @@
 import React, { useEffect, useState } from 'react';
-import { Route, Switch, Link } from 'react-router-dom'
+import { withRouter, Route, Switch, Link } from 'react-router-dom'
 import axios from 'axios'
 
-export default function PostDetail(props) {
-  console.log("post detail", props)
-  // const filteredPost = props.props.find(data => data.id === props.match.params.id)
-  // console.log(filteredPost)
+function PostDetail(props) {
+  console.log("post detail", props.data)
+  console.log("second post", props)
+  const filteredPost = props.data.find((post) => post._id === props.match.params.id)
+
+  console.log(filteredPost)
 
 
   return (
     <div className="postDetail">
-      {/* <h1>{filteredPost.title}</h1> */}
-      {/* @Felicia: I left the height and width here but you can change it to whatever you want so it's a main image like in a post  */}
-      {/* <img src={props.img} height="200px" width="auto" />
-      <h2>Author: {props.author}</h2>
-      <p>{props.post}</p> */}
+      {filteredPost && <div>
+        <div>{filteredPost.title}</div>
+        {/* @Felicia: I left the height and width here but you can change it to whatever you want so it's a main image like in a post  */}
+        <img src={filteredPost.img} height="200px" width="auto" />
+        <h2>Author: {filteredPost.author}</h2>
+
+        <div>{filteredPost.post}</div>
+
+      </div>
+      }
     </div>
+
   )
+
 }
+
+export default withRouter(PostDetail)
